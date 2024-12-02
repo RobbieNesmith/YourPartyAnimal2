6c305ada0e14334717b4b59ddc39bd131fa2af10
Robbie Nesmith <bobberto1995@gmail.com>
2024-12-01 18:03:59 -0600

    Add functions in QueueService for getting songs

--- app/services/QueueService.ts
+++ app/services/QueueService.ts
@@ -8,3 +8,19 @@ export async function enqueueSong(userId: number, id: string, name: string) {
     requested_at: new Date(),
     rating: 0}});
 }
+
+export async function getNowPlaying(userId: number) {
+  return await prisma.song.findFirst({where: {
+    user_id: userId,
+    played_at: {not: null}
+  },
+  orderBy: {played_at: "desc"}});
+}
+
+export async function getQueuedSongs(userId: number) {
+  return await prisma.song.findMany({where: {
+    user_id: userId,
+    played_at: null
+  },
+  orderBy: {requested_at: "asc"}});
+}
