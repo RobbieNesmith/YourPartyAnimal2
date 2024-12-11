1caa2d24abb100253235a106af9123228be26314
Robbie Nesmith <bobberto1995@gmail.com>
2024-12-10 20:56:29 -0600

    More party page styling

--- app/components/GuestSongQueue.tsx
+++ app/components/GuestSongQueue.tsx
@@ -6,7 +6,12 @@ export default function GuestSongQueue({queuedSongs}: {queuedSongs: Song[]}) {
     <ol className="queueList">
       {queuedSongs.map(s => {
         return (
-          <li className="queueItem" key={s.id}>{s.name}</li>
+          <li className="queueItem" key={s.id}>
+            <img className="thumbnail" src={`https://i.ytimg.com/vi/${s.video_id}/hqdefault.jpg`} />
+            <div className="centerContainer">
+              <span>{s.name}</span>
+            </div>
+          </li>
         );
       })}
     </ol>
