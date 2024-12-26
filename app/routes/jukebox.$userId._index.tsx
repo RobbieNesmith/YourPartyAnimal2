641e5dba2a0f8c6d767b63ecc2932b35e77fd7d1
Robbie Nesmith <bobberto1995@gmail.com>
2024-12-26 16:02:05 -0600

    Set up jukebox with react-youtube

--- app/routes/jukebox.$userId._index.tsx
+++ app/routes/jukebox.$userId._index.tsx
@@ -1,7 +1,14 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { useCallback, useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import { prisma } from "~/prisma.server";
 import { getNowPlaying, getQueuedSongs } from "~/services/QueueService";
+
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const YouTubeElement: typeof YouTube = YouTube.default ?? YouTube;
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const idNum = parseInt(params.userId || "0");
@@ -16,10 +23,33 @@ export async function loader({ params }: LoaderFunctionArgs) {
 
 export default function DjPartyView() {
   const {user, nowPlaying, queuedSongs} = useLoaderData<typeof loader>();
+  const [actualNowPlaying, setActualNowPlaying] = useState(nowPlaying);
+  const [actualQueue, setActualQueue] = useState(queuedSongs);
+
+  const getNextSong = useCallback(() => {
+    const nextSong = actualQueue[0];
+    setActualQueue(aq => aq.slice(1));
+    setActualNowPlaying(nextSong);
+  }, [actualQueue, setActualNowPlaying, setActualQueue]);
+
+  useEffect(() => {
+    if (!actualNowPlaying) {
+      getNextSong();
+    }
+  }, [actualNowPlaying]);
+
+  if (!actualNowPlaying) {
+    return "Nothing to see here";
+  }
 
   return (
     <div>
       <h1>{user.name}'s Party</h1>
+      <YouTubeElement
+        opts={{playerVars: {autoplay: 1}}}
+        videoId={actualNowPlaying?.video_id}
+        onEnd={getNextSong}
+      />
     </div>
   )
 }
