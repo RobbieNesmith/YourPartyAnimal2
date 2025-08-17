ba37277e30929d9d1ae854f814314485a9a7eeea
Robbie Nesmith <bobberto1995@gmail.com>
2025-08-17 16:33:39 -0500

    Add another promotion test

--- test/QueueService.test.ts
+++ test/QueueService.test.ts
@@ -1,7 +1,7 @@
 import { Song } from "@prisma/client";
 import { computeNowPlayingOrder } from "~/services/QueueService";
 
-test("Voting Affects Now Playing Order", () => {
+test("Positive voting decreases index", () => {
   const song: Song = {
     name: "Test Song Oh So Good",
     id: 12345,
@@ -13,4 +13,18 @@ test("Voting Affects Now Playing Order", () => {
   }
   const songWithIndex = computeNowPlayingOrder(song, 4, 7);
   expect(songWithIndex.index).toBe(2.5);
+});
+
+test("Negative voting increases index", () => {
+  const song: Song = {
+    name: "Test Song Oh So Good",
+    id: 12345,
+    user_id: 1,
+    video_id: "n1ceOne",
+    rating: -9,
+    requested_at: new Date("2025-01-01T12:00"),
+    played_at: null
+  }
+  const songWithIndex = computeNowPlayingOrder(song, 4, 7);
+  expect(songWithIndex.index).toBe(5.5);
 });
