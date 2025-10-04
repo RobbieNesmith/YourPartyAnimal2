896bebfeb799fb6c271da1cf157644e7c8addd97
Robbie Nesmith <bobberto1995@gmail.com>
2025-10-04 16:21:49 -0500

    Update tests with new fields

--- test/QueueService.test.ts
+++ test/QueueService.test.ts
@@ -9,7 +9,9 @@ test("Positive voting decreases index", () => {
     video_id: "n1ceOne",
     rating: 9,
     requested_at: new Date("2025-01-01T12:00"),
-    played_at: null
+    played_at: null,
+    preset: false,
+    requested_by: "this-should-be-a-guid",
   }
   const songWithIndex = computeNowPlayingOrder(song, 4, 7);
   expect(songWithIndex.index).toBe(2.5);
@@ -23,7 +25,9 @@ test("Negative voting increases index", () => {
     video_id: "n1ceOne",
     rating: -9,
     requested_at: new Date("2025-01-01T12:00"),
-    played_at: null
+    played_at: null,
+    preset: false,
+    requested_by: "this-should-be-a-guid",
   }
   const songWithIndex = computeNowPlayingOrder(song, 4, 7);
   expect(songWithIndex.index).toBe(5.5);
