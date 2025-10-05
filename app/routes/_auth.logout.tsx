f54d27468aba9110d52551a66f98c959fd90b650
Robbie Nesmith <bobberto1995@gmail.com>
2025-10-05 16:20:16 -0500

    Destroy session on logout

--- app/routes/_auth.logout.tsx
+++ app/routes/_auth.logout.tsx
@@ -1,14 +1,25 @@
+import { ActionFunctionArgs } from "@remix-run/node";
 import { redirect } from "remix-typedjson";
+import { secretSession } from "~/services/AuthService.server";
 
-export const action = async () => {
+export async function action({request}: ActionFunctionArgs) {
   const {
     AUTH0_DOMAIN,
     AUTH0_CLIENT_ID,
     BASE_URL
   } = process.env;
 
+  const session = await secretSession.getSession(
+    request.headers.get("Cookie")
+  );
+
   throw redirect(
-    `https://${AUTH0_DOMAIN}/v2/logout?client_id=${AUTH0_CLIENT_ID}&returnTo=${BASE_URL}`
+    `https://${AUTH0_DOMAIN}/v2/logout?client_id=${AUTH0_CLIENT_ID}&returnTo=${BASE_URL}`,
+    {
+      headers: {
+        "Set-Cookie": await secretSession.destroySession(session),
+      },
+    }
   );
 }
 
