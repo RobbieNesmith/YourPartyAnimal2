ff80d4dc806298e427770f801b50566b130b10d7
Robbie Nesmith <bobberto1995@gmail.com>
2025-10-15 21:39:34 -0500

    Display search result lengths

--- app/components/SearchResultsEntry.tsx
+++ app/components/SearchResultsEntry.tsx
@@ -9,11 +9,20 @@ interface SearchResultsEntryProps {
   destination: "queue" | "preset";
 }
 
+interface VideoLength {
+  accessibility: {
+    accessibilityData: {
+      label: string
+    }
+  }
+  simpleText: string;
+}
+
 export default function SearchResultsEntry({partyId, item, destination}: SearchResultsEntryProps) {
   const buttonText = destination == "queue" ? "Add to Queue" : "Add Preset";
 
   return (
-    <ListItem key={item.id!} imageUrl={`https://i.ytimg.com/vi/${item.id}/hqdefault.jpg`} title={item.title!}>
+    <ListItem key={item.id!} imageUrl={`https://i.ytimg.com/vi/${item.id}/hqdefault.jpg`} title={`${item.title!} - ${(item.length as unknown as VideoLength).simpleText}`}>
       <form action={`/party/${partyId}/${destination}/add`} method="POST">
         <input type="hidden" name="id" value={item.id} />
         <input type="hidden" name="title" value={item.title} />
